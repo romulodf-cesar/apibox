e108db8241482697a3227034b21e8e037a2d677c
romulodf-cesar <romulodf.cesar@gmail.com>
2026-05-28 21:40:33 -0300

    ok

--- apibox_app/admin.py
+++ apibox_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+# registrar o modelo Box para que ele apareça na interface de administração do Django
+from .models import Box
+# faça para mostrar o nome e o número da caixa na interface de administração
+class BoxAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'numero')
+    search_fields=('numero',)
+admin.site.register(Box, BoxAdmin)
 
-# Register your models here.
