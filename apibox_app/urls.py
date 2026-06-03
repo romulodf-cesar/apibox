b16755cd26b2789d05a13a303247d0eeba182d84
romulodf-cesar <romulodf.cesar@gmail.com>
2026-06-03 19:20:29 -0300

    ok

--- apibox_app/urls.py
+++ apibox_app/urls.py
@@ -1,6 +1,25 @@
+from django.urls import path,include
+from .views import BoxViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('boxes',BoxViewSet,basename='box')
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
+
+
+
+
+"""
 from django.urls import path
 from apibox_app.views import box
 
 urlpatterns = [
     path('box/', box, name='box'),
 ]
+
+
+"""
--- apibox_app/views.py
+++ apibox_app/views.py
@@ -1,8 +1,25 @@
+from apibox_app.models import Box
+from apibox_app.serializers import BoxSerializer
+from rest_framework import viewsets 
+
+class BoxViewSet(viewsets.ModelViewSet):
+    queryset = Box.objects.all()
+    serializer_class = BoxSerializer
+
+
+
+
+
+
+
+
+
 """
   HTTP -> Hypertext Transfer Protocol
   HTTPS -> Hypertext Transfer Protocol Secure
   JSON -> JavaScript Object Notation
   https://www.w3schools.com/js/js_json.asp
+"""
 """
 
 from django.http import JsonResponse
@@ -15,3 +32,7 @@ def box(request):
             'numero': 101,
         }
         return JsonResponse(box_feira)
+
+
+
+"""
