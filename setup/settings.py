8d5bdd2d462b480e74551f34916271510b657e0e
romulodf-cesar <romulodf.cesar@gmail.com>
2026-06-03 21:10:48 -0300

    ok

--- setup/settings.py
+++ setup/settings.py
@@ -75,6 +75,8 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -90,6 +92,17 @@ DATABASES = {
         }
     }
 }
+
+"""
+
+# Altere a configuração para o SQLite:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 
 # Password validation
